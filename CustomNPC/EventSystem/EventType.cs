ff95031ed631015d3814b7568bdad6b350c9cccb
C0sm0s-chill <Nitr0x0fficiel@gmail.com>
2022-02-20 11:57:25 +0100

    Ajoutez des fichiers projet.

---
+++ CustomNPC/EventSystem/EventType.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomNPC.EventSystem
+{
+    public enum EventType
+    {
+        None = 0,
+
+        PluginUpdate,
+        PostPluginUpdate,
+
+        NpcUpdate,
+        PostNpcUpdate,
+
+        NpcCollision,
+        NpcDamage,
+        NpcKill,
+        ServerChat
+    }
+}
---
+++ CustomNPC/EventSystem/Events/NpcKilledEvent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace CustomNPC.EventSystem.Events
+{
+    public class NpcKilledEvent
+    {
+        public int NpcIndex { get; set; }
+
+        public int PlayerIndex { get; set; }
+
+        public int Damage { get; set; }
+
+        public float Knockback { get; set; }
+
+        public byte Direction { get; set; }
+
+        public bool CriticalHit { get; set; }
+
+        public Vector2 LastPosition { get; set; }
+    }
+}
